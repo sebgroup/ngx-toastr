3172a2dca985440ae980930ad6fa573ce0ef1bd4
Robert Hjalmers <robert.hjalmers@gmail.com>
2019-11-11 16:57:05 +0100

    test: fix basic tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,26 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { ExemplifyModule } from 'angular-exemplify';
+import { ToastrModule, ToastContainerModule } from 'ngx-toastr';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
         AppComponent
+      ],
+      imports: [
+        ExemplifyModule,
+        ToastrModule.forRoot({
+          timeOut: 10000,
+          closeButton: true,
+          disableTimeOut: false,
+          progressBar: true,
+          toastClass: 'toast',
+          positionClass: 'toast-bottom-right',
+          enableHtml: true
+        }),
+        ToastContainerModule,
       ],
     }).compileComponents();
   }));
@@ -16,16 +31,22 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ngx-toastr'`, () => {
+  it(`should have a empty title ''`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('ngx-toastr');
+    expect(app.title).toEqual('');
+  });
+
+  it(`should have a empty message ''`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app.message).toEqual('');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to ngx-toastr!');
+    expect(compiled.querySelector('h1').textContent).toContain('@sebgroup/ngx-toastr');
   });
 });
